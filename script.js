e5975e0d2a4c132aa51d5c5aa01f0ff9fda683d0
Ayush Mehrotra <ayushmehrotra099@gmail.com>
2024-02-19 17:41:59 +0530

    added left right movements

--- script.js
+++ script.js
@@ -6,6 +6,18 @@ document.onkeydown = function (e) {
     setTimeout(() => {
       dino.classList.remove("animateDino");
     }, 700);
+  } else if (e.keyCode == 39) {
+    dino = document.querySelector(".dino");
+    dinoX = parseInt(
+      window.getComputedStyle(dino, null).getPropertyValue("left")
+    );
+    dino.style.left = dinoX + 100 + "px";
+  } else if (e.keyCode == 37) {
+    dino = document.querySelector(".dino");
+    dinoX = parseInt(
+      window.getComputedStyle(dino, null).getPropertyValue("left")
+    );
+    dino.style.left = dinoX + -100 + "px";
   }
 };
 
@@ -13,4 +25,15 @@ setInterval(() => {
   dino = document.querySelector(".dino");
   gameOver = document.querySelector(".gameOver");
   obstacle = document.querySelector(".obstacle");
+  dx = parseInt(window.getComputedStyle(dino, null).getPropertyValue("left"));
+  dy = parseInt(window.getComputedStyle(dino, null).getPropertyValue("top"));
+
+  ox = parseInt(window.getComputedStyle(dino, null).getPropertyValue("left"));
+  oy = parseInt(window.getComputedStyle(dino, null).getPropertyValue("top"));
+  offsetX = Math.abs(dx - ox);
+  offsetY = Math.abs(dy - oy);
+  if (offsetX < 93 && offsetY < 52) {
+    gameOver.style.visibility = "visible";
+    obstacle.classList.remove("obstacleAni");
+  }
 }, 100);
