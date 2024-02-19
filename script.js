d22d4be0526ea73dd374b68719a7d138d166c603
Ayush Mehrotra <ayushmehrotra099@gmail.com>
2024-02-19 16:37:11 +0530

    marioJumper 50% done

---
+++ script.js
@@ -0,0 +1,16 @@
+document.onkeydown = function (e) {
+  console.log(e.keyCode);
+  if (e.keyCode == 38) {
+    dino = document.querySelector(".dino");
+    dino.classList.add("animateDino");
+    setTimeout(() => {
+      dino.classList.remove("animateDino");
+    }, 700);
+  }
+};
+
+setInterval(() => {
+  dino = document.querySelector(".dino");
+  gameOver = document.querySelector(".gameOver");
+  obstacle = document.querySelector(".obstacle");
+}, 100);
